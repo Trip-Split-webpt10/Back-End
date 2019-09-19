a86f1fe7af837fa52ecc012d5006fbaef6edfc60
cwern40 <chris.wernli@hotmail.com>
2019-09-18 19:13:11 -0600

    I created the migrations file with the first migration. I coded it to create 5 tables, but before I do that I will have this checked to see if I need 5 tables or if there would be a way to meet MVP and create less tables

---
+++ data/migrations/20190918184332_trips-create.js
@@ -0,0 +1,71 @@
+
+exports.up = function(knex) {
+    return knex.schema.createTable('users', users => {
+        users.increments();
+        users.string('username', 128).notNullable().unique();
+        users.string('password', 128).notNullable();
+    })
+    .createTable('trips', trips => {
+        trips.increments();
+        trips.string('destination').notNullable();
+        trips.boolean('complete').defaultTo(false);
+        trips.date('start_date');
+        trips.date('end_date');
+    })
+    .createTable('expenses', expenses => {
+        expenses.increments();
+        expenses.string('name').notNullable();
+        expenses.decimal('price');
+        expenses.integer('trip_id')
+            .notNullable()
+            .unsigned()
+            .references('id')
+            .inTable('trips')
+            .onDelete('CASCADE')
+            .onUpdate('CASCADE')
+    })
+    .createTable('user_trips', tbl => {
+        tbl.integer('user_id')
+            .unsigned()
+            .notNullable()
+            .references('id')
+            .inTable('users')
+            .onDelete('CASCADE')
+            .onUpdate('CASCADE')
+        tbl.integer('trip_id')
+            .unsigned()
+            .notNullable()
+            .references('id')
+            .inTable('trips')
+            .onDelete('CASCADE')
+            .onUpdate('CASCADE')
+        tbl.primary(['user_id', 'trip_id'])
+    })
+    .createTable('user_expenses', tbl => {
+        tbl.integer('user_id')
+            .unsigned()
+            .notNullable()
+            .references('id')
+            .inTable('users')
+            .onDelete('CASCADE')
+            .onUpdate('CASCADE')
+        tbl.integer('expense_id')
+            .unsigned()
+            .notNullable()
+            .references('id')
+            .inTable('expenses')
+            .onDelete('CASCADE')
+            .onUpdate('CASCADE')
+        tbl.decimal('amount')
+        tbl.primary(['user_id', 'expense_id'])
+    })
+};
+
+exports.down = function(knex) {
+    return knex.schema
+        .dropTableIfExists('users')
+        .dropTableIfExists('trips')
+        .dropTableIfExists('expenses')
+        .dropTableIfExists('user_trips')
+        .dropTableIfExists('user_expenses')
+};
