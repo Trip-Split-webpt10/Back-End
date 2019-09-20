189a096e5be1865906c959ac604f0f1d0b6a81dd
cwern40 <chris.wernli@hotmail.com>
2019-09-19 19:22:29 -0600

    I added the endpoints for the users

--- data/seeds/01-users-create.js
+++ data/seeds/01-users-create.js
@@ -5,9 +5,9 @@ exports.seed = function(knex) {
     .then(function () {
       // Inserts seed entries
       return knex('users').insert([
-        {name: 'Chris Test', username: 'chris', password: 'password'},
-        {name: 'User Test', username: 'username', password: 'password'},
-        {name: 'John Doe', username: 'john', password: 'password'}
+        {name: 'Chris Test', username: 'chris', password: '$2a$10$d6FbScul1zrlWZNTE5HYDOGm9T3fMg5mfbKVnXbiz60SA1vXFJ9HK'},
+        {name: 'User Test', username: 'username', password: '$2a$10$Vq.BFt9GRvH9YUN8G/R/A.KovtLpPEZW/mooFtOMpUl3fpQi3YWim'},
+        {name: 'John Doe', username: 'john', password: '$2a$10$0b.SWt3Hc86mz17pNEcP7.IKlLS6q4qyuy.94p.FUPaVQivMv.nOG'}
       ]);
     });
 };
--- users/user-router.js
+++ users/user-router.js
@@ -1,3 +1,93 @@
 const router = require('express').Router();
+const bcrypt = require('bcryptjs');
+
+const Users = require('./users-model');
+
+router.get('/:id', (req, res) => {
+    const { id } = req.params;
+
+    Users.findById(id)
+        .then(user => {
+            if (user) {
+                res.json(user);
+            } else {
+                res.status(404).json({
+                    message: 'Could not find user by that id'
+                })
+            }
+        })
+        .catch(err => {
+            res.status(500).json({
+                message: 'Failed to get user'
+            })
+        })
+})
+
+router.post('/register', (req, res) => {
+    let user = req.body;
+    const hash = bcrypt.hashSync(user.password, 10);
+    user.password = hash;
+
+    Users.add(user)
+        .then(saved => {
+            res.status(201).json(saved)
+        })
+        .catch(err => {
+            console.log(err)
+            res.status(500).json(err)
+        })
+})
+
+router.post('/login', (req, res) => {
+    let { username, password } = req.body;
+
+    Users.findBy({ username })
+        .first()
+        .then(user => {
+            if (user && bcrypt.compareSync(password, user.password)) {
+                res.status(200).json({
+                    message: 'You have successfully logged in'
+                })
+            } else {
+                res.status(401).json({
+                    message: 'Incorrect username or password'
+                })
+            }
+        })
+        .catch(err => {
+            console.log(err)
+            res.status(500).json(err)
+        })
+})
+
+router.put('/:id', (req, res) => {
+    const { id } = req.params;
+    const changes = req.body;
+    let user = req.body;
+    const hash = bcrypt.hashSync(user.password, 10);
+    user.password = hash;
+
+    Users.findById(id)
+        .then(user => {
+            if (user) {
+                Users.update(changes, id)
+                    .then(updatedUser => {
+                        res.json(updatedUser)
+                    })
+                    .catch(err => {
+                        console.log(err)
+                        res.status(500).json(err)
+                    })
+            } else {
+                res.status(404).json({
+                    message: 'Could not find user with given id'
+                })
+            }
+        })
+        .catch(err => {
+            console.log(err)
+            res.status(500).json(err)
+        })
+})
 
 module.exports = router;
